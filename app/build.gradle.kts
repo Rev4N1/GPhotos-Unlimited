fbdb2680b3317584077a5d276890d86b12135ce6
osm0sis <osm0sis@outlook.com>
2026-09-09 10:17:09 -0300

    Update NDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.rev4n.unlimitedphotos"
     compileSdk = 37
-    ndkVersion = "29.0.14206865"
+    ndkVersion = "30.0.16248370"
     buildToolsVersion = "37.0.0"
 
     buildFeatures {
